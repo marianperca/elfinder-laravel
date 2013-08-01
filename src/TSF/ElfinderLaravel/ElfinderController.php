6cd6ba108f86761bd61dc7cfe63d55c5d29c7f40
Marian Perca <marian.perca@gmail.com>
2013-08-01 17:05:57 +0300

    removed default thumb folder

--- src/TSF/ElfinderLaravel/ElfinderController.php
+++ src/TSF/ElfinderLaravel/ElfinderController.php
@@ -24,7 +24,7 @@ class ElfinderController extends \BaseController {
         if(!file_exists(public_path()."/$dir/js/i18n/elfinder.$locale.js")){
             $locale = false;
         }
-        return \View::make('elfinder-laravel::ckeditr')->with(compact('dir', 'locale'));
+        return \View::make('elfinder-laravel::ckeditor')->with(compact('dir', 'locale'));
     }
 
     public function showConnector()
@@ -39,8 +39,7 @@ class ElfinderController extends \BaseController {
                     'driver'        => 'LocalFileSystem',                       // driver for accessing file system (REQUIRED)
                     'path'          => public_path().DIRECTORY_SEPARATOR.$dir,  // path to files (REQUIRED)
                     'URL'           => asset($dir),                             // URL to files (REQUIRED)
-                    'accessControl' => \Config::get('elfinder-laravel::access'),        // filter callback (OPTIONAL)
-                    'tmbPath'       => 'thumbnails'                             // thumbnails path
+                    'accessControl' => \Config::get('elfinder-laravel::access') // filter callback (OPTIONAL)
                 )
             );
         }
